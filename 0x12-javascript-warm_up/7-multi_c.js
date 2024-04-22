e19c4c4af253a500e88f66df7304440ef0187820
Victortimbwa <victortimbwa@gmail.com>
2024-04-22 23:16:44 +0300

    Updates

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 const arg = process.argv[2];
 const num = parseInt(arg);
 
-if (isNan(num)) {
- console.log('Missing number of occurrences');
- } else {
-	for (let i = 0; i < num; i++){
-		console.log('C is fun');
-	}
- }
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
