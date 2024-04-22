1db76b252c96aada561a668e67b0f3bcabafef05
Victortimbwa <victortimbwa@gmail.com>
2024-04-22 23:04:59 +0300

    An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 const arg = process.argv[2];
 const num = parseInt(arg);
 
-if (isNan(num)) {
- console.log('Not a number');
- } else {
- console.log('My number: ' + num);
- }
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
+}
