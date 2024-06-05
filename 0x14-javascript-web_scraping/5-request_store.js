4a10de19e232183d7573630a84204d14710720eb
Victortimbwa <victortimbwa@gmail.com>
2024-06-06 01:31:57 +0300

    A script that gets contents of a webpage

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+if (process.argv.length !== 4) {
+  console.error('Usage: node fetchAndStore.js <URL> <file_path>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log(`Successfully saved the contents of ${url} to ${filePath}`);
+      }
+    });
+  }
+});
