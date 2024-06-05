a98e77591993e272d5c8d95a030ea70ca879a71e
Victortimbwa <victortimbwa@gmail.com>
2024-06-05 05:42:07 +0300

    C is fun

--- 0x14-javascript-web_scraping/readme.js
+++ 0x14-javascript-web_scraping/readme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-
+const file = process.argv[2];
 const fs = require('fs');
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
     console.error(err);
   } else {
