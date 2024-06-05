b0eea775dc218fa3cca4a7f13127f494614c80f2
Victortimbwa <victortimbwa@gmail.com>
2024-06-05 20:20:16 +0300

    Updated API

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,26 +1,16 @@
 #!/usr/bin/node
+
 const request = require('request');
-const movieId = process.argv[2];
+const episode = process.argv[2];
+const apiurl = 'https://swapi-api.alx-tools.com/api/films/';
 
-if (!movieId) {
-  console.log('Please provide the movie ID as the first argument.');
-  process.exit(1);
-}
-
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    process.exit(1);
+request(apiurl + episode, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responsejsobj = JSON.parse(body);
+    console.log(responsejsobj.title);
+  } else {
+    console.log('code: ' + response.StatusCode);
   }
-
-  if (response.statusCode !== 200) {
-    console.log('Failed to retrieve data. Please check the movie ID.');
-    process.exit(1);
-  }
-
-  const movieData = JSON.parse(body);
-  const movieTitle = movieData.title;
-
-  console.log(`The title of the Star Wars movie with ID ${movieId} is: ${movieTitle}`);
 });
