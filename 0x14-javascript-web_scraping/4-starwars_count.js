01e17c92eb529091ae8b7a72e50f5661c293940d
Victortimbwa <victortimbwa@gmail.com>
2024-06-05 21:05:19 +0300

    Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(apiUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        const films = JSON.parse(body);
+
+        const characterId = 18;
+        const moviesWithWedge = films.results.filter((film) => {
+            return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
+        });
+        console.log(`Number of movies with Wedge Antilles:${moviesWithWedge.lenght}`);
+    } else {
+        console.error('Failed to fetch data');
+    }
+});
