3e56ed6bbf1d9cd69e6edcce634a11d978293a95
Victortimbwa <victortimbwa@gmail.com>
2024-06-05 17:30:57 +0300

    Short two

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
-
+const file = process.argv[2];
+const string = process.argv[3];
 const fs = require('fs');
 
-const [filePath, content] = process.argv.slice(2);
-
-if (!filePath || !content) {
-    console.log('Please provide both file path and content to write.');
-    process.exit(1);
-}
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('Content has been written to the file successfully.');
-    }
+fs.writeFile(file, string, 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
